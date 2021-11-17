0993da6f9b2ee11fb122b6f4a6d76c88671ee591
btoker <burak.toker@logiwa.com>
2021-11-17 20:37:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace KafkaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var config = new ConsumerConfig { GroupId = "product-consumer", BootstrapServers = "kafka:9092"};
+                using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+                {
+                    consumer.Subscribe("127.0.0.1.dbo.Product");
+                    while (true)
+                    {
+                        ConsumeResult<string, string> consumeResult = consumer.Consume();
+                        Console.WriteLine($"Message {consumeResult.TopicPartitionOffset}:{consumeResult.Value}");
+                    consumer.Commit();
+                }
+            }
+            }
+            catch (System.Exception ex)
+            {
+            Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
